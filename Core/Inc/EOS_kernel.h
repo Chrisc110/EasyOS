bac21dbf5179aec083aab4b297429e3152df2203
Christopher Chung <tps90067@gmail.com>
2022-09-14 22:05:32 -0500

    add thread functions and data structs

--- Core/Inc/EOS_kernel.h
+++ Core/Inc/EOS_kernel.h
@@ -2,7 +2,9 @@
 #define EOS_KERNEL_H_
 
 
+void EOSkernelInitialize();
 
+void EOSkernelStart();
 
 
 #endif /* EOS_KERNEL_H_ */
--- Core/Inc/EOS_thread.h
+++ Core/Inc/EOS_thread.h
@@ -1,6 +1,8 @@
 #ifndef EOS_THREAD_H_
 #define EOS_THREAD_H_
 
+EOSstatus_t * EOSstatus;
+
 typedef enum {
 	HIGHEST_PRIORITY = 0,
 	BELOW_HIGHEST_PRIORITY,
@@ -8,16 +10,36 @@ typedef enum {
 	BELOW_MEDIUM_PRIORITY,
 	LOW_PRIORITY,
 	LOWEST_PRIORITY
-} EOStaskPriority;
+} EOStaskPriority_e;
 
 typedef enum {
 	RUNNING,
 	READY,
 	WAITING,
 	SLEEPING
-} EOStaskState;
+} EOStaskState_e;
 
-typedef struct EOSthread {
-
+typedef struct EOSthread_t {
+	void (*funct_ptr)();
+	uint32_t * stackPtrAddr;
+	EOStaskPriority priority;
+	uint32_t delay;
+	struct EOSthread * next;
 };
+
+typedef struct {
+	EOSthread * runningThread;
+	uint8_t threadCount;
+	uint32_t sysCount;
+	EOSthread * priorityArray[6];
+	EOSthread * sleepingHead;
+	uint32_t * initialMSP;
+	//mutexHead
+	//semaphoreHead
+
+}EOSstatus_t;
+
+void EOScreateThread (void (*funct_ptr)(), EOStaskPriority priority);
+
+
 #endif /* EOS_THREAD_H_ */
